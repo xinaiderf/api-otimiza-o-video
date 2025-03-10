31de9ca9554b428cd569af4fd3835b6ab8eb87c9
Gabriel Schneider <67759197+xinaiderf@users.noreply.github.com>
2025-03-10 18:57:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ async def compress_video_api(video: UploadFile = File(...), crf: int = 28):
         os.remove(temp_output_video)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8010)
+    uvicorn.run(app, host="0.0.0.0", port=8011)
