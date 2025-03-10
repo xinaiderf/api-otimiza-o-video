b29b222c8aebc0c3c15b44c902be7e5d53ac3268
Gabriel Schneider <67759197+xinaiderf@users.noreply.github.com>
2025-03-10 19:01:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,26 +24,27 @@ def compress_video(input_video_path, output_video_path, crf_value=28):
 
 @app.post("/compress/")
 async def compress_video_api(video: UploadFile = File(...), crf: int = 28):
-    # Criação de arquivos temporários
-    temp_input_video = tempfile.mktemp(suffix='.mp4')
-    temp_output_video = tempfile.mktemp(suffix='.mp4')
+    # Usando NamedTemporaryFile para garantir que o arquivo temporário seja criado corretamente
+    with tempfile.NamedTemporaryFile(suffix='.mp4', delete=False) as temp_input_video:
+        temp_input_video.write(await video.read())
+        temp_input_video_path = temp_input_video.name
     
-    # Salvar o arquivo de vídeo enviado para o servidor
-    with open(temp_input_video, "wb") as f:
-        f.write(await video.read())
+    # Usando NamedTemporaryFile para o arquivo de saída
+    with tempfile.NamedTemporaryFile(suffix='.mp4', delete=False) as temp_output_video:
+        temp_output_video_path = temp_output_video.name
     
     try:
         # Chamar a função para compactar o vídeo
-        compress_video(temp_input_video, temp_output_video, crf)
+        compress_video(temp_input_video_path, temp_output_video_path, crf)
         
         # Retornar o vídeo comprimido como resposta
-        return FileResponse(temp_output_video, media_type='video/mp4', filename='compressed_video.mp4')
+        return FileResponse(temp_output_video_path, media_type='video/mp4', filename='compressed_video.mp4')
     except Exception as e:
         return {"error": str(e)}
     finally:
         # Limpeza dos arquivos temporários
-        os.remove(temp_input_video)
-        os.remove(temp_output_video)
+        os.remove(temp_input_video_path)
+        os.remove(temp_output_video_path)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8011)
