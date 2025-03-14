8cbb6e0d3cb8ca7888e40467c564834155a4f9a1
Gabriel Schneider <67759197+xinaiderf@users.noreply.github.com>
2025-03-14 00:35:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -78,4 +78,4 @@ async def optimize_video(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8011, reload=True)
